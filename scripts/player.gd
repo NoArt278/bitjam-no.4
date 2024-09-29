3f9cd8b307b072853fa0fcf14ea5ad1e1f42a80c
Nat10k <156871636+NoArt278@users.noreply.github.com>
2024-09-29 20:59:19 +0700

    feat : change player movement, color materials

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,16 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+const ACCELERATION = 50.0
+const DECELERATION = 1.0
 const JUMP_VELOCITY = 4.5
+const SHEAR_SPEED = 40.0
+const SHEAR_ANGLE = 20.0
+const MAX_SPEED = 30.0
 var mouse_sensitivity : float = 0.002
 var mouse_pos_delta : Vector2 = Vector2.ZERO
-@onready var camera_3d: Camera3D = $Camera3D
-
+@onready var camera_anchor: Node3D = $CameraAnchor
+@onready var camera_3d: Camera3D = $CameraAnchor/Camera3D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -22,18 +26,24 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x += direction.x * ACCELERATION * delta
+		velocity.z += direction.z * ACCELERATION * delta
+		if abs(velocity.x) > MAX_SPEED :
+			velocity.x = MAX_SPEED * direction.x
+		if abs(velocity.z) > MAX_SPEED :
+			velocity.z = MAX_SPEED * direction.z
+		camera_anchor.rotation_degrees.z = move_toward(camera_anchor.rotation_degrees.z, SHEAR_ANGLE * input_dir.x * -1, SHEAR_SPEED * delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, DECELERATION)
+		velocity.z = move_toward(velocity.z, 0, DECELERATION)
+		camera_anchor.rotation_degrees.z = move_toward(camera_anchor.rotation_degrees.z, 0, SHEAR_SPEED * delta)
 
 	move_and_slide()
 	
 	if mouse_pos_delta.length() < 50 :
 		rotate_y(mouse_pos_delta.x * mouse_sensitivity)
-		$Camera3D.rotate_x(mouse_pos_delta.y * mouse_sensitivity)
-		$Camera3D.rotation.x = clampf($Camera3D.rotation.x, -deg_to_rad(70), deg_to_rad(70))
+		camera_3d.rotate_x(mouse_pos_delta.y * mouse_sensitivity)
+		camera_3d.rotation.x = clampf(camera_3d.rotation.x, -deg_to_rad(70), deg_to_rad(70))
 	
 	mouse_pos_delta = Vector2.ZERO
 
